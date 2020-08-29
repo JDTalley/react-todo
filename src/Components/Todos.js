ae88c5e423547b49b1af890e6e86c3c661f13000
Dakota <dakota.talley@gmail.com>
2020-08-28 22:43:59 -0400

    Added check functionality. Added todos array.

--- src/Components/Todos.js
+++ src/Components/Todos.js
@@ -1,19 +1,61 @@
 import React from 'react'
 
-import ListItem from './ListItem'
+import TodoItem from './TodoItem'
 
 class Todos extends React.Component {
     constructor() {
         super()
+        this.state = {
+            todos: [
+                {
+                    id: 1,
+                    text: 'Todo 1',
+                    completed: true
+                },
+                {
+                    id: 2,
+                    text: 'Todo 2',
+                    completed: false
+                },
+                {
+                    id: 3,
+                    text: 'Todo 3',
+                    completed: false
+                }
+            ]
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(id) {
+        this.setState(prevState => {
+            const updatedTodos = prevState.todos.map(todo => {
+                if (todo.id === id) {
+                    return {
+                        ...todo,
+                        completed: !todo.completed
+                    }
+                }
+                return todo
+            })
+            return {
+                todos: updatedTodos
+            }
+        })
     }
 
     render() {
+        const todoItems = this.state.todos.map(item => 
+            <TodoItem 
+                key={item.id} 
+                item={item}
+                handleChange={this.handleChange}
+            />)
+
         return (
             <div>
                 <h1>Todos</h1>
-                <ListItem />
-                <ListItem />
-                <ListItem />
+                {todoItems}
             </div>
         )
     }
