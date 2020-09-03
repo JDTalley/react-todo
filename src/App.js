bf0f85a8ddaefb83212c2986e224a1a5bcee8c6f
Dakota Talley <dakota.talley@gmail.com>
2020-09-02 22:46:33 -0400

    Added container styles.

--- src/App.js
+++ src/App.js
@@ -4,8 +4,22 @@ import './App.css';
 import Todos from './Components/Todos'
 
 function App() {
+  let style = {
+    height: '100vh',
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    background: '#0f0c29',
+    background: '-webkit-linear-gradient(to right, #24243e, #302b63, #0f0c29)',
+    background: 'linear-gradient(to right, #24243e, #302b63, #0f0c29)'
+  
+  }
+
   return (
-    <Todos />
+    <div style={style}>
+      <Todos />
+    </div>
   );
 }
 
